cdb0b6cb3b0fb7a002c90364f3dd94f47bf52aed
Max Drljić <max_spree@hotmail.com>
2018-01-16 08:31:37 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize';
 const sequelize = new Sequelize(
   'react-apollo-graphql',
   'postgres',
-  'zagrebutrine',
+  'yourpasswordgoeshere',
   {
     host: 'localhost',
     dialect: 'postgres',
